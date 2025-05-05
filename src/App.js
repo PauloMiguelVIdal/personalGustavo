199ef9d4e19d807a9304ace3f76401d5a1bbf2b3
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-05-04 21:31:40 -0300

    feito alterações nos planos

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Home from './components/Home';
 import PlanosIndividuais from './components/PlanosIndividuais';
 import BasePlanos from './components/BasePlanos';
 import Depoimentos from './components/Depoimentos';
+import Depoimentos2 from './components/Depoimentos2';
 import SobreMim from './components/SobreMim';
 import PlanosConsultoria from './components/PlanosConsultoria';
+import PlanosGerais from './components/PlanosGerais';
 // import About from './components/About';
 // import Contact from './components/Contact';
 // import Services from './components/Services';
@@ -15,11 +17,13 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <BasePlanos/>
-      <PlanosIndividuais />
-      <Depoimentos/>
-<SobreMim/>
-<PlanosConsultoria/>
+      {/* <BasePlanos /> */}
+      {/* <PlanosIndividuais /> */}
+      {/* <Depoimentos /> */}
+      <Depoimentos2 />
+      <SobreMim />
+      <PlanosGerais />
+      <PlanosConsultoria />
     </div>
   );
 }
--- src/components/PlanosConsultoria.jsx
+++ src/components/PlanosConsultoria.jsx
@@ -1,50 +1,176 @@
 import React from 'react';
 
 const PlanosConsultoria = () => {
-  const planos = [
-    {
-        titulo: 'Pacote Mensal Musculação',
-        preco: 'R$ 197',
-        beneficios: 
-        ['treinos são individuais, específicos para seu caso, objetivo e necessidade.', 
-             'Cardápio Alimentar para melhorar seus resultados, de acordo com seu objetivo',
-             'Suporte diário para tirar qualquer dúvida',],
-    },
-    {
-        titulo: 'Pacote Trimestral Musculação',
-        preco: 'R$ 297',
-        beneficios: ['Benefício 1', 'Benefício 2', 'Benefício 3', 'Benefício 4'],
-    },
-    {
-      titulo: 'Pacote Semestral Musculação',
-        preco: 'R$ 497',
-        beneficios: ['Benefício 1', 'Benefício 2', 'Benefício 3', 'Benefício 4', 'Benefício 5'],
-    },
-    {
-        titulo: '5X p/ Semana',
-        preco: 'R$ 699,90/mês',
-        beneficios: ['Benefício 1', 'Benefício 2', 'Benefício 3', 'Benefício 4', 'Benefício 5'],
-    },
-];
+    const planos = [
+        {
+            titulo: 'Mensal',
+            descTitle: "Somente Treino",
+            desc: "Para quem quer ter a primeira experiência com a sua melhor versão",
+            preco: 197,
+            precoReal: 197,
+            descount: 0,
+            cores: {
+                bgCor1: "F23005",
+                bgCor2: "F23005",
+                bgTitle1: "000000",
+                bgTitle2: "000000",
+                bgPrice1: "000000",
+                bgPrice2: "000000",
+                colorDescount: "000000",
+                bgBotton1: "000000",
+                bgBotton2: "000000",
+                colorTextBotton: "FFFFFF",
+            }
 
-return (
-  <div className="planos-container">
-    <h1>Planos de Consultoria Online</h1>
-    <p>Escolha o plano que melhor se adapta às suas necessidades e objetivos.</p>
-      {planos.map((plano, index) => (
-          <div className="card" key={index}>
-              <h2 className='titulo'>{plano.titulo}</h2>
-              <p className="preco">{plano.preco}</p>
-              {/* <ul className='base-planos-container'>
-                  {plano.beneficios.map((beneficio, idx) => (
-                      <li className='features' key={idx}>{beneficio}</li>
-                  ))}
-              </ul> */}
-              <button className='botão'>Assinar</button>
-          </div>
-      ))}
-  </div>
-);
+        },
+        {
+            titulo: 'Mensal',
+            descTitle: "Treino + Dieta",
+            desc: "Para quem quer ter a primeira experiência com a sua melhor versão",
+            preco: 397,
+            precoReal: 397,
+            descount: 0,
+            cores: {
+                bgCor1: "F23005",
+                bgCor2: "8C0403",
+                bgTitle1: "000000",
+                bgTitle2: "000000",
+                bgPrice1: "000000",
+                bgPrice2: "000000",
+                colorDescount: "000000",
+                bgBotton1: "F23005",
+                bgBotton2: "F23005",
+                colorTextBotton: "FFFFFF",
+            }
+
+        },
+        {
+            titulo: 'Trimestral',
+            descTitle: "Somente treino",
+            desc: "Perfeito para quem busca consistência e quer começar a ver resultados reais",
+            preco: 297,
+            precoReal: 590,
+            descount: 15,
+            cores: {
+                bgCor1: "F23005",
+                bgCor2: "F20705",
+                bgTitle1: "F25A05",
+                bgTitle2: "F25A05",
+                bgPrice1: "F25A05",
+                bgPrice2: "F20705",
+                colorDescount: "F28705",
+                bgBotton1: "F23005",
+                bgBotton2: "F23005",
+                colorTextBotton: "FFFFFF",
+            }
+
+        },
+        {
+            titulo: 'Trimestral',
+            descTitle: "Treino + Dieta",
+            desc: "Perfeito para quem busca consistência e quer começar a ver resultados reais",
+            preco: 470,
+            precoReal: 1182,
+            descount: 20,
+            cores: {
+                bgCor1: "F25A05",
+                bgCor2: "F20705",
+                bgTitle1: "F20705",
+                bgTitle2: "8C0403",
+                bgPrice1: "F20705",
+                bgPrice2: "8C0403",
+                colorDescount: "8C0403",
+                bgBotton1: "8C0403",
+                bgBotton2: "8C0403",
+                colorTextBotton: "FFFFFF",
+            }
+
+        },
+        {
+            titulo: 'Semestral',
+            descTitle: "Somente Treino",
+            desc: "Para quem está decidido a transformar de vez seu corpo e sua rotina.",
+            preco: 590,
+            precoReal: 1182,
+            descount: 50,
+            cores: {
+                bgCor1: "F29F05",
+                bgCor2: "F2C36B",
+                bgTitle1: "F25A05",
+                bgTitle2: "F25A05",
+                bgPrice1: "F20705",
+                bgPrice2: "F28705",
+                colorDescount: "F23005",
+                bgBotton1: "F20705",
+                bgBotton2: "F20705",
+                colorTextBotton: "FFFFFF",
+            }
+
+        },
+        {
+            titulo: 'Semestral',
+            descTitle: "Treino + Dieta",
+            desc: "Para quem está decidido a transformar de vez seu corpo e sua rotina.",
+            preco: 997,
+            precoReal: 2364,
+            descount: 57,
+            cores: {
+                bgCor1: "F29F05",
+                bgCor2: "8C3403",
+                bgTitle1: "F25A05",
+                bgTitle2: "8C3403",
+                bgPrice1: "F20705",
+                bgPrice2: "F28705",
+                colorDescount: "FFFFFF",
+                bgBotton1: "F29F05",
+                bgBotton2: "F2C36B",
+                colorTextBotton: "F20705",
+            }
+
+        },
+    ];
+
+    return (
+        <div className="planos-container">
+            <div className="containerSpaceAdjust"></div>
+            {planos.map((plano, index) => (
+                <div className="package-card" key={index} style={{ background: `linear-gradient(135deg,#${plano.cores.bgCor1}, #${plano.cores.bgCor2})` }}>
+                    <div className='containerTitlePackage' style={{ background: `linear-gradient(135deg,#${plano.cores.bgTitle1}, #${plano.cores.bgTitle2})` }}>
+                        <h2 className="titlePackage" style={{ color: `#FFFFFF` }}>{plano.titulo}</h2>
+                    </div>
+                    <h3 className="descTitle" style={{ color: `#FFFFFF` }}>{plano.descTitle}</h3>
+
+                    <p className="desc" style={{ color: `#FFFFFF` }}>{plano.desc}</p>
+                    
+                    
+                    <p className='descountText'>{plano.preco !== plano.precoReal && (
+                        <h3 className="descTitlePrice">R$ {plano.precoReal},00</h3>
+                    )}
+                            </p>
+                    <p className='descountText'>{plano.descount > 0 && (
+                        <span className="desconto" style={{ color: `#${plano.cores.colorDescount}` }}>
+                            {" "}({plano.descount}% OFF)
+                        </span>
+                    )}
+                    </p>
+                    <div className="containerPricePackage" style={{ background: `linear-gradient(135deg,#${plano.cores.bgPrice1}, #${plano.cores.bgPrice2})`, marginBottom: '20px' }}>
+                        <p className="priceText" style={{ color: `#FFFFFF` }}>
+                            R$ {plano.preco},00
+                        </p>
+                    </div>
+                    <button
+                        className="bottonPackage"
+                        style={{
+                            background: `linear-gradient(135deg,#${plano.cores.bgBotton1}, #${plano.cores.bgBotton2})`,
+                            color: `#${plano.cores.colorTextBotton}`
+                        }}
+                    >
+                        Comprar
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
 
 
     // const planos = [
@@ -66,23 +192,23 @@ return (
     //     },
     // ];
 
-//     return (
-// <div className="container-planos">
-//   <h1>Planos de Consultoria Online</h1>
-//   {planos.map((plano, index) => (
-//     <div key={index} className="plano-box">
-//       <h2>{plano.tipo}</h2>
-//       <ul>
-//         {plano.opcoes.map((opcao, idx) => (
-//           <li key={idx}>
-//             <strong>{opcao.duracao}:</strong> R$ {opcao.preco},00
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   ))}
-// </div>
-//     );
+    //     return (
+    // <div className="container-planos">
+    //   <h1>Planos de Consultoria Online</h1>
+    //   {planos.map((plano, index) => (
+    //     <div key={index} className="plano-box">
+    //       <h2>{plano.tipo}</h2>
+    //       <ul>
+    //         {plano.opcoes.map((opcao, idx) => (
+    //           <li key={idx}>
+    //             <strong>{opcao.duracao}:</strong> R$ {opcao.preco},00
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   ))}
+    // </div>
+    //     );
 };
 
 export default PlanosConsultoria;
